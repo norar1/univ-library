c7f1119616612036c70aa1907b69fe6815d89647
norar17 <arviedavid313@gmail.com>
2025-01-19 02:56:03 +0800

    binalik lang

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -2,6 +2,8 @@ import config from "@/lib/config";
 import ImageKit from "imagekit";
 import { NextResponse } from "next/server";
 
+
+
 const {
   env: {
     imagekit: { publicKey, privateKey, urlEndpoint },
@@ -15,12 +17,5 @@ const imagekit = new ImageKit({
 });
 
 export async function GET() {
-  const response = NextResponse.json(imagekit.getAuthenticationParameters());
-
-  // Allow all origins
-  response.headers.set('Access-Control-Allow-Origin', '*');  // Public access
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allowed methods
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
-
-  return response;
+  return NextResponse.json(imagekit.getAuthenticationParameters());
 }
